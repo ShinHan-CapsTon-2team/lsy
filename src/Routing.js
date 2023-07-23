a4d272c2663fbcb3dc585bf0bb86fd7467a68b5e
ddooing <stpaq@naver.com>
2023-07-23 22:04:34 +0900

    routing js 추가

--- src/Routing.js
+++ src/Routing.js
@@ -9,6 +9,8 @@ import QuizReult from './Quiz/Quizresult'
 import Post from './PostUp/Post'
 import Home from './Home/homepage.js'
 import Reco from './PostUp/recommend.js'
+import Lookup from './Lookup/Look_up.js' // 조회
+
 function Routing() {
   return (
       <div classNamer="App">
@@ -25,6 +27,7 @@ function Routing() {
             <Route path ='/quizresult' element={<QuizReult />}/>
 
             <Route path ='/home' element={<Home />}/>
+            <Route path ='/lookup' element={<Lookup/>}/>
 
 
           </Routes>
