d4687e80b9ae0c72b6cc557a14211f3f1df9a36f
ddooing <stpaq@naver.com>
2023-08-25 17:05:05 +0900

    prfileicon 관련 주석

---
+++ src/Modal/ProandLogout.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+const DropMenu = styled.div` 
+  position: absolute;
+  background-color: white;
+  //border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 30px;
+  z-index: 9999;
+
+  //text-align: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  top: 100px;
+  right:80px;
+
+
+  /* tablet 규격 */
+        @media screen and (max-width: 1023px){
+            
+        }
+
+        /* mobile 규격 */
+        @media screen and (max-width: 540px){
+          width:45vw;
+          //top: -147px;
+        }
+        /* s 데스크 */
+        @media screen and (min-width: 1024px){
+          width:15vw;
+        }
+        /* l 데스크 */
+        @media screen and (min-width: 1700px){
+          width:20vw;
+          top: 120px;
+        }
+`;
+
+const CateMenu = styled.div` 
+  font-size: 25px;
+  margin-top:5px;
+
+  /* tablet 규격 */
+        @media screen and (max-width: 1023px){
+            
+        }
+
+        /* mobile 규격 */
+        @media screen and (max-width: 540px){
+            
+        }
+        /* s 데스크 */
+        @media screen and (min-width: 1024px){
+          
+        }
+        /* l 데스크 */
+        @media screen and (min-width: 1700px){
+          font-size: 30px;
+        }
+`;
+
+export const ProandLogout = () => {
+    // 자기 프로필 가는거 처리하기 App js 참고  
+  const onGoProfile = () => {
+       
+  };
+  // 로그아웃 처리하기 
+  const onNaverLogout = () => {
+     
+  };
+    return
+    (
+
+    <DropMenu>
+        <CateMenu onClick={onGoProfile}>마이프로필</CateMenu>  
+        <CateMenu onClick={onNaverLogout}> 로그아웃 </CateMenu>          
+    </DropMenu>
+    );
+};
--- src/Modal/ProfileIcon.js
+++ src/Modal/ProfileIcon.js
@@ -143,41 +143,35 @@ const CateMenu = styled.div`
 `;
 
 
-
+//예시 !!!!!!! 알아보기 쉽게 
 export const ProfileIcon = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false); // 모달창때문에 있는거 삭제 노 
+
+    const [isLogin, setIsLogin] = useState(false); //로그인 유무 따지기 
   
-    const openModalHandler = () => {
-      // isOpen의 상태를 변경하는 메소드를 구현
-      // !false -> !true -> !false
+    const openModalHandler = () => { // 모달창 관련임 자세히 알 필요 X 
       setIsOpen(!isOpen) 
     };
 
-   
-  // 자기 프로필 가는거 처리하기 App js 참고  
-  const onGoProfile = () => {
-       
-  };
-  // 로그아웃 처리하기 
-  const onNaverLogout = () => {
-     
-  };
+  
     return (
       <>
         <HomeWrap>
-          <HomeLogo src={profilelogo} onClick={openModalHandler}
-          // 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다. 
-          /> 
-            {/* 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때는 ModalBtn의 내부 텍스트가 'Opened!' 로 Modal이 닫힌 상태(isOpen이 false인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal'이 되도록 구현 */}
-          
-          {/* 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현 */}
-          {isOpen ? 
-          <DropMenu>
-            <CateMenu onClick={onGoProfile}>마이프로필</CateMenu>  
-            <CateMenu onClick={onNaverLogout}> 로그아웃 </CateMenu>          
-          </DropMenu>
-            : null
-          }
+          <HomeLogo src={profilelogo} onClick={openModalHandler} />
+          {isLogin ?  // 로그인 유무 여부 처리를 해야하는 거임 
+          ( // 1. 로그인 하기 전 상태라면 
+            isOpen ? (
+              <ModalBackdrop onClick={openModalHandler}>
+                <LoginModal />
+              </ModalBackdrop>
+            ) : null
+          ) : (
+            //2. 로그인한 후 상태라면 
+            <DropMenu>
+              <CateMenu onClick={onGoProfile}>마이프로필</CateMenu>  
+              <CateMenu onClick={onNaverLogout}>로그아웃</CateMenu>          
+            </DropMenu>
+          )}
         </HomeWrap>
       </>
     );
