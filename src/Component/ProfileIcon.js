2a9ad921e8b3b770030f5208b3994c23c26748ce
youjung <kelly000128@naver.com>
2023-09-17 20:30:34 +0900

    0917 profileIcon 수정

--- src/Component/ProfileIcon.js
+++ src/Component/ProfileIcon.js
@@ -6,6 +6,7 @@ import * as S from "./ProfileWrapStyle";
 import { ModalBackdrop } from "../Modal/ModalStyle";
 
 export const ProfileIcon = () => {
+  const [userinfo, setUserinfo] = useState([]);
   const [isOpen, setIsOpen] = useState(false); // 모달창때문에 있는거 삭제 노
   const accessToken = localStorage.getItem("access_token"); // 로컬 스토리지에서 액세스 토큰 가져오기
   const openModalHandler = () => {
@@ -14,9 +15,37 @@ export const ProfileIcon = () => {
   };
   const navigate = useNavigate();
 
-  // 자기 프로필 가는거 처리하기 App js 참고
+
+  // 자기 프로필 가는거 처리하기 App js 참고  
   const onGoProfile = () => {
-    navigate("/profile");
+    // 서버로 액세스 토큰을 보내서 사용자 이메일 정보를 요청
+    const accessToken = localStorage.getItem("access_token");
+  
+    if (accessToken) {
+      fetch('http://localhost:4001/api/user', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ accessToken }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUserinfo(data);
+          console.log("현재 접속중인 사용자 이메일:", data.email);
+          console.log("현재 접속중인 사용자 닉네임:", data.nickname);
+  
+          // 이메일 아이디 추출
+          const emailParts = data.email.split('@');
+          const emailId = emailParts[0];
+  
+          // 이메일 아이디를 가지고 프로필 페이지로 이동
+          navigate(`/profile/${emailId}`);
+        })
+        .catch((error) => {
+          console.error('Error fetching user email:', error);
+        });
+    }
   };
 
   const onNaverLogout = () => {
